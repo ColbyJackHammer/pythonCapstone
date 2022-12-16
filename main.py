61b36f66a6b4a8f7b2daaede42835f632af8ae47
James Austin <james.colby.austin@gmail.com>
2022-12-16 09:42:54 -0600

    created workhorse function to execute querries

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import mysql.connector
 from mysql.connector import Error
 
 
-def create_server_connection(host_name, user_name, user_password):
+def create_server_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
-        )
+            passwd=user_password,
+            database=db_name,
+
         print("MySQL Database connection successful")
     except Error as err:
         print(f"Error: '{err}'")
@@ -26,7 +27,15 @@ def create_database(connection, query):
 
 create_database_query = "CREATE DATABASE Hospitall"
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error '{err}'")
 
 
-connection = create_server_connection("localhost", "root", "student")
+connection = create_server_connection("localhost", "root", "student","hospitall")
 create_database(connection, create_database_query)
